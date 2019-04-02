98e39f544b4ea8a3f763d6c506e17a276a544f84
jandevera <30372125+jandevera@users.noreply.github.com>
2019-04-02 20:32:39 +0800

    first draft using ReactJS

---
+++ precache-manifest.473af4689f7ff02f68518926a69738b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae7e3f52c56bc1642602",
+    "url": "/static/js/main.ae7e3f52.chunk.js"
+  },
+  {
+    "revision": "53fb117074ad8dcf4f80",
+    "url": "/static/js/1.53fb1170.chunk.js"
+  },
+  {
+    "revision": "ae7e3f52c56bc1642602",
+    "url": "/static/css/main.eba72c17.chunk.css"
+  },
+  {
+    "revision": "a7469e077a110b610d056e24bd62f734",
+    "url": "/index.html"
+  }
+];
